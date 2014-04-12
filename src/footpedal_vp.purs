f3e6ba439580409b9bd5023c3de73e7776d30699
Dan Connolly <http://www.madmode.com/>
2014-04-12 13:24:55 -0500

    update decode w.r.t. safe !!   - Note `sequence [Just 1]` is OK but `sequence $ [Just 1] is not`.     See purescript/issues/376.

--- src/footpedal_vp.purs
+++ src/footpedal_vp.purs
@@ -1,12 +1,27 @@
-module Main where
+module Footpedal where
 
 import Data.Array
+import Data.Maybe
+import Data.Traversable
+import Data.Tuple
+import Control.Applicative
 
 import Debug.Trace
 
 main = trace "Hello, World!"
 
-decode :: [Number] -> [{pedal:: Number, pressed:: Number}]
-decode chunk = do ix <- range 0 3
-                  return { pedal: chunk !! (ix * 8), pressed: chunk !! (ix * 8 + 4) }
+type PedalState = {pedal:: Number, pressed:: Number}
+
+decode :: [Number] -> Maybe [PedalState]
+decode bytes = sequence do
+  ix <- range 0 2
+  let offset = ix * 8
+  return $ parts offset
+  where
+    parts :: Number -> Maybe PedalState
+    parts offset = do
+                 pedal_num <- bytes !! offset
+                 state <- bytes !! (offset + 4)
+                 return { pedal: pedal_num, pressed: state }
+
 
