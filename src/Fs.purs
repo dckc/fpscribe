62533cbb4dc06bcc65f198f388cff71dacd2785a
Dan Connolly <http://www.madmode.com/>
2014-04-13 17:07:53 -0500

    - never mind putting require() in Exception effect  - fix js typos in createReadStream

--- src/Fs.purs
+++ src/Fs.purs
@@ -1,26 +1,25 @@
 module Fs where
 
 import Control.Monad.Eff
-import Control.Monad.Eff.Exception
 import Data.Foreign
 import Data.Maybe
 
 import qualified Node.Events as Ev
 
-foreign import require_fs "require 'fs'" ::
-    forall jex e. Eff (ex :: Exception jex | e) FsModule
+foreign import fs "var fs = require('fs')" :: FsModule
 foreign import data FsModule :: *
 
 -- http://nodejs.org/api/fs.html#fs_fs_createreadstream_path_options
 -- TODO: 
 foreign import createReadStream
-  "function(fs){\
+  "function createReadStream(fs){\
   \  return function(path){\
   \    return function (options){\
   \      return function (){\
   \        return fs.createReadStream(path, options);\
-  \    }\
-  \  }\
+  \      };\
+  \    };\
+  \  };\
   \}" :: forall eff opts d. FsModule -> String -> { | opts } ->
          Eff (n :: Ev.Node | eff) (Ev.EventEmitter d)
 
